025b34ec0cb53cfca9f2ee61a8f60ec79b039041
Hideki Abe <hideki.abe.0630@gmail.com>
2018-07-03 20:54:26 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Separate Content
+
+WordPressの投稿本文を`<!--more-->`の前後で分割します。  
+Movable Typeの`MTEntryBody`, `MTEntryMore`相当の働きを実現します。
+
+## 使い方
+
+### moreより前を出力
+
+```
+<?php SeparateContent::the_content_of_body(); ?>
+```
+
+Movable Typeの`MTEntryBody`相当です。
+
+### moreより後を出力
+
+```
+<?php SeparateContent::the_content_of_more(); ?>
+```
+
+Movable Typeの`MTEntryMore`相当です。
+
+## 実装参考
+
+`the_content()`の実装を参考にしました。
+
+- [wp-includes/post-template.php](https://core.trac.wordpress.org/browser/tags/4.9.6/src/wp-includes/post-template.php)
---
+++ wp-separate-content.php
@@ -0,0 +1,61 @@
+<?php
+/*
+Plugin Name: WP Separate Content
+Description: Separate content by more tag.
+Version: 1.0.0
+Author: Hideki Abe
+Author URI: https://www.anothersky.pw/
+Copyright: Hideki Abe
+*/
+
+if ( ! class_exists( 'WPSeparateContent' ) ) :
+
+class WPSeparateContent {
+	function __construct() {
+
+		/* Do nothing here */
+
+	}
+
+	private function separate_content( $content ) {
+		preg_match('/([\w\W]*)<!--more(.*?)?-->([\w\W]*)/', $content, $matches);
+		return $matches;
+	}
+
+	public static function the_content_of_body() {
+		$post = get_post();
+		$content = $post->post_content;
+		$matches = WPSeparateContent::separate_content( $content );
+
+		if ( $matches ) {
+			$content_of_body = $matches[1];
+
+			if ( ! empty( $content_of_body ) ) {
+				$content_of_body = apply_filters( 'the_content', $content_of_body );
+				echo $content_of_body;
+				return;
+			}
+		}
+
+		$content_of_body = apply_filters( 'the_content', $content );
+		echo $content_of_body;
+	}
+
+	public static function the_content_of_more() {
+		$post = get_post();
+		$content = $post->post_content;
+		$matches = WPSeparateContent::separate_content( $content );
+
+		if ( $matches ) {
+			$content_of_more = $matches[3];
+
+			if ( ! empty( $content_of_more ) ) {
+				$content_of_more = apply_filters( 'the_content', $content_of_more );
+				echo $content_of_more;
+				return;
+			}
+		}
+	}
+}
+
+endif;
